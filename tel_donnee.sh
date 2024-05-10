2f31b4107af4e3f6ea74b4479b8208c91ec72f70
tux <mathieu.barrios@outlook.com>
2024-05-10 18:43:17 +0200

    modif preparation des fichiers pour avoir les valeurs par rapport aux classes d'age + fait des graphes dans analyses + ajout d'un fichier dans le tel_donnee pour avoir la population francaise

--- tel_donnee.sh
+++ tel_donnee.sh
@@ -6,3 +6,4 @@ rm *.xlsx
 echo 'telechargement des données'
 curl -X GET 'https://data.ameli.fr/api/explore/v2.1/catalog/datasets/effectifs/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B' >> effectifs.csv
 curl -X GET 'https://data.ameli.fr/api/explore/v2.1/catalog/datasets/depenses/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B'  >> depenses.csv
+curl -X GET 'https://www.insee.fr/fr/statistiques/fichier/7746154/fe_dod_compo_crois.xlsx'  >> pop.xlsx
